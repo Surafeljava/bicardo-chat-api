07a4cdaabe4f312e117a180b89e89eabebc8279e
root <surafelk27@gmail.com>
2022-07-02 20:28:01 +0000

    feat: mongo connection done

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 express = require('express');
+const mongoose = require("mongoose");
 app = express();
+app.use(express.json());
+
+
+mongoose.connect('mongodb://localhost:27017/bicardochat',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+).then(() => { console.log('Mongo Connected') }).catch(e => console.log(`Error Message: ${e}`));
+
+
+app.use('/', (res, req) => {
+    req.send("Bicardo Chat API . . .");
+})
+
 
 app.listen(5000, () => {
     console.log("Listening on PORT - 5000");
